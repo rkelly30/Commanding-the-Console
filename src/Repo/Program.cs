14605b54984710e1695da0dbeb553b2eddc4dc02
rkelly30 <46577129+rkelly30@users.noreply.github.com>
2019-09-05 11:53:14 -0600

    Initial proof of concept re#2

--- src/Repo/Program.cs
+++ src/Repo/Program.cs
@@ -10,9 +10,14 @@ namespace Repo
 {
     class Program
     {
+        const string API_BASE = "https://www.gitignore.io/api/";
+        const string API_LINES = API_BASE + "list?format=lines";
         static void Main(string[] args)
         {
-            WriteLine("Hello World");
+            // Use the HttpClient to grab content form a website
+            var client = new HttpClient();
+            var template = client.GetAsync(API_LINES).GetAwaiter().GetResult();
+            WriteLine(template);
         }
     }
 }
